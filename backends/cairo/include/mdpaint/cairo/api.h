4af641c65af664a4af1d1cf81932d3f37c9bf7d5
Mateusz Drost <mat.drost@gmail.com>
2025-12-16 15:36:07 +0100

    cairo changes

--- backends/cairo/include/mdpaint/cairo/api.h
+++ backends/cairo/include/mdpaint/cairo/api.h
@@ -3,10 +3,14 @@
 
 #include <mdpaint/api.h>
 
-#ifdef MDP_CAIRO_EXPORTS
-# define MDP_CAIRO_API MDP_INTERFACE_API_EXPORT
+#ifdef MDP_CAIRO_BUILD_SHARED
+# ifdef MDP_CAIRO_EXPORTS
+#  define MDP_CAIRO_API MDP_INTERFACE_API_EXPORT
+# else
+#  define MDP_CAIRO_API MDP_INTERFACE_API_IMPORT
+# endif
 #else
-# define MDP_CAIRO_API MDP_INTERFACE_API_IMPORT
+# define MDP_CAIRO_API
 #endif
 
 #endif // MDP_CAIRO_API_H
