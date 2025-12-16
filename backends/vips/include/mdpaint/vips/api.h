aa61efa59ac33d04a6ea0074f94ebe9344df2da7
Mateusz Drost <mat.drost@gmail.com>
2025-12-16 15:36:22 +0100

    vips changes

--- backends/vips/include/mdpaint/vips/api.h
+++ backends/vips/include/mdpaint/vips/api.h
@@ -3,10 +3,14 @@
 
 #include <mdpaint/api.h>
 
-#ifdef MDP_VIPS_EXPORTS
-# define MDP_VIPS_API MDP_INTERFACE_API_EXPORT
+#ifdef MDP_VIPS_BUILD_SHARED
+# ifdef MDP_VIPS_EXPORTS
+#  define MDP_VIPS_API MDP_INTERFACE_API_EXPORT
+# else
+#  define MDP_VIPS_API MDP_INTERFACE_API_IMPORT
+# endif
 #else
-# define MDP_VIPS_API MDP_INTERFACE_API_IMPORT
+# define MDP_VIPS_API
 #endif
 
 #endif // MDP_VIPS_API_H
