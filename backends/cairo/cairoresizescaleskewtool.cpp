c800c7d7539ab9d1a3a04b26cfa5c71762b46917
Mateusz Drost <mat.drost@gmail.com>
2025-12-17 13:15:13 +0100

    cairo changes

--- backends/cairo/cairoresizescaleskewtool.cpp
+++ backends/cairo/cairoresizescaleskewtool.cpp
@@ -5,6 +5,7 @@
 
 #include <mdpaint/history.h>
 
+#include <cmath>
 #include <functional>
 #include <stdexcept>
 
@@ -61,48 +62,63 @@ void mdpCairoResizeScaleSkewTool::leaveEvent() /* override */
 // public virtual
 void mdpCairoResizeScaleSkewTool::resizeScaleSkew(const mdpResizeScaleSkewData& resizeScaleSkewData) /* override */
 {
-    const double x = -resizeScaleSkewData.x;
-    const double y = -resizeScaleSkewData.y;
-    const int newWidth = resizeScaleSkewData.width;
-    const int newHeight = resizeScaleSkewData.height;
     cairo_surface_t* previewSurface = m_cairoModel.getPreviewSurface();
+    const int imageWidth = cairo_image_surface_get_width(previewSurface);
+    const int imageHeight = cairo_image_surface_get_height(previewSurface);
+    const double x = resizeScaleSkewData.x;
+    const double y = resizeScaleSkewData.y;
+    const int width = resizeScaleSkewData.width;
+    const int height = resizeScaleSkewData.height;
+    const double skewX = resizeScaleSkewData.skewX;
+    const double skewY = resizeScaleSkewData.skewY;
+    const  double tanSkewX = std::tan(skewX);
+    const  double tanSkewY = std::tan(skewY);
+    double scaleX;
+    double scaleY;
+    double offsetX = -std::min(tanSkewY * width, 0.0);
+    double offsetY = -std::min(tanSkewX * height, 0.0);
+    if (resizeScaleSkewData.scale) {
+        scaleX = width / (double)imageWidth;
+        scaleY = height / (double)imageHeight;
+        offsetX += x;
+        offsetY += y;
+    }
+    else {
+        scaleX = 1.0;
+        scaleY = 1.0;
+    }
+    const int newWidth = width + std::abs(tanSkewY) * width;
+    const int newHeight = height + std::abs(tanSkewX) * height;
     cairo_surface_t* newPreviewSurface = cairo_surface_create_similar_image(
         previewSurface,
         cairo_image_surface_get_format(previewSurface),
         newWidth,
         newHeight
     );
-    if (const cairo_status_t status = cairo_surface_status(newPreviewSurface); status != CAIRO_STATUS_SUCCESS) {
+    cairo_t* newPreviewContext = cairo_create(newPreviewSurface);
+    cairo_save(newPreviewContext);
+    cairo_set_operator(newPreviewContext, CAIRO_OPERATOR_SOURCE);
+    cairo_set_source_surface(newPreviewContext, previewSurface, 0.0, 0.0);
+    cairo_pattern_t* pattern = cairo_get_source(newPreviewContext);
+    cairo_pattern_set_filter(pattern, CAIRO_FILTER_BEST);
+    cairo_matrix_t matrix;
+    double xx = scaleX;
+    double xy = scaleX * tanSkewY;
+    double yx = scaleY * tanSkewX;
+    double yy = scaleY;
+    cairo_matrix_init(&matrix, xx, yx, xy, yy, offsetX, offsetY);
+    if (const cairo_status_t status = cairo_matrix_invert(&matrix); status != CAIRO_STATUS_SUCCESS) {
+        cairo_destroy(newPreviewContext);
+        cairo_surface_destroy(newPreviewSurface);
         throw std::runtime_error(cairo_status_to_string(status));
     }
-    cairo_t* newPreviewContext = cairo_create(newPreviewSurface);
-    if (resizeScaleSkewData.scale) {
-        const int width = cairo_image_surface_get_width(previewSurface);
-        const int height = cairo_image_surface_get_height(previewSurface);
-        const double xScale = newWidth / (double)width;
-        const double yScale = newHeight / (double)height;
-        cairo_save(newPreviewContext);
-        cairo_scale(newPreviewContext, xScale, yScale);
-        cairo_set_operator(newPreviewContext, CAIRO_OPERATOR_SOURCE);
-        cairo_set_source_surface(newPreviewContext, previewSurface, 0.0, 0.0);
-        cairo_pattern_t* pattern = cairo_get_source(newPreviewContext);
-        cairo_pattern_set_filter(pattern, CAIRO_FILTER_BEST);
-        cairo_pattern_set_extend(pattern, CAIRO_EXTEND_PAD);
-        if (const cairo_status_t status = cairo_pattern_status(pattern); status != CAIRO_STATUS_SUCCESS) {
-            throw std::runtime_error(cairo_status_to_string(status));
-        }
-        cairo_paint(newPreviewContext);
-        cairo_restore(newPreviewContext);
-    }
-    else {
-        cairo_set_operator(newPreviewContext, CAIRO_OPERATOR_SOURCE);
-        cairo_set_source_rgb(newPreviewContext, 1.0, 1.0, 1.0);
-        cairo_paint(newPreviewContext);
-        cairo_set_operator(newPreviewContext, CAIRO_OPERATOR_OVER);
-        cairo_set_source_surface(newPreviewContext, previewSurface, x, y);
-        cairo_paint(newPreviewContext);
-    }
+    cairo_pattern_set_matrix(pattern, &matrix);
+    cairo_set_source(newPreviewContext, pattern);
+    cairo_paint(newPreviewContext);
+    cairo_restore(newPreviewContext);
     if (const cairo_status_t status = cairo_status(newPreviewContext); status != CAIRO_STATUS_SUCCESS) {
+        cairo_destroy(newPreviewContext);
+        cairo_surface_destroy(newPreviewSurface);
         throw std::runtime_error(cairo_status_to_string(status));
     }
     try {
