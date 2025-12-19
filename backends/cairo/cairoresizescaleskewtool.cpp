e71e80c2d33ae2a18f1c49da23a6a4a44ed3d2f3
Mateusz Drost <mat.drost@gmail.com>
2025-12-19 23:22:22 +0000

    cairo changes

--- backends/cairo/cairoresizescaleskewtool.cpp
+++ backends/cairo/cairoresizescaleskewtool.cpp
@@ -75,8 +75,8 @@ void mdpCairoResizeScaleSkewTool::resizeScaleSkew(const mdpResizeScaleSkewData& 
     const  double tanSkewY = std::tan(skewY);
     double scaleX;
     double scaleY;
-    double offsetX = -std::min(tanSkewY * height, 0.0);
-    double offsetY = -std::min(tanSkewX * width, 0.0);
+    double offsetX = -std::min(tanSkewX * height, 0.0);
+    double offsetY = -std::min(tanSkewY * width, 0.0);
     if (resizeScaleSkewData.scale) {
         scaleX = width / (double)imageWidth;
         scaleY = height / (double)imageHeight;
@@ -87,8 +87,17 @@ void mdpCairoResizeScaleSkewTool::resizeScaleSkew(const mdpResizeScaleSkewData& 
         offsetX -= x;
         offsetY -= y;
     }
-    const int newWidth = width + std::abs(tanSkewY) * height;
-    const int newHeight = height + std::abs(tanSkewX) * width;
+    const int newWidth = width + std::abs(tanSkewX) * height;
+    const int newHeight = height + std::abs(tanSkewY) * width;
+    cairo_matrix_t matrix;
+    const double xx = scaleX;
+    const double xy = scaleY * tanSkewX;
+    const double yx = scaleX * tanSkewY;
+    const double yy = scaleY;
+    cairo_matrix_init(&matrix, xx, yx, xy, yy, offsetX, offsetY);
+    if (const cairo_status_t status = cairo_matrix_invert(&matrix); status != CAIRO_STATUS_SUCCESS) {
+        throw std::runtime_error(cairo_status_to_string(status));
+    }
     cairo_surface_t* newPreviewSurface = cairo_surface_create_similar_image(
         previewSurface,
         cairo_image_surface_get_format(previewSurface),
@@ -96,26 +105,13 @@ void mdpCairoResizeScaleSkewTool::resizeScaleSkew(const mdpResizeScaleSkewData& 
         newHeight
     );
     cairo_t* newPreviewContext = cairo_create(newPreviewSurface);
-    cairo_save(newPreviewContext);
     cairo_set_operator(newPreviewContext, CAIRO_OPERATOR_SOURCE);
     cairo_set_source_surface(newPreviewContext, previewSurface, 0.0, 0.0);
     cairo_pattern_t* pattern = cairo_get_source(newPreviewContext);
     cairo_pattern_set_filter(pattern, CAIRO_FILTER_BEST);
-    cairo_matrix_t matrix;
-    double xx = scaleX;
-    double xy = scaleY * tanSkewY;
-    double yx = scaleX * tanSkewX;
-    double yy = scaleY;
-    cairo_matrix_init(&matrix, xx, yx, xy, yy, offsetX, offsetY);
-    if (const cairo_status_t status = cairo_matrix_invert(&matrix); status != CAIRO_STATUS_SUCCESS) {
-        cairo_destroy(newPreviewContext);
-        cairo_surface_destroy(newPreviewSurface);
-        throw std::runtime_error(cairo_status_to_string(status));
-    }
     cairo_pattern_set_matrix(pattern, &matrix);
     cairo_set_source(newPreviewContext, pattern);
     cairo_paint(newPreviewContext);
-    cairo_restore(newPreviewContext);
     if (const cairo_status_t status = cairo_status(newPreviewContext); status != CAIRO_STATUS_SUCCESS) {
         cairo_destroy(newPreviewContext);
         cairo_surface_destroy(newPreviewSurface);
